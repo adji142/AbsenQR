8b47775572e9382f515a373d6baca14597187c1a
adji142 <prasetyoajiw@gmail.com>
2022-12-19 22:17:27 +0700

    fix dashboard

--- application/views/Dashboard.php
+++ application/views/Dashboard.php
@@ -9,54 +9,6 @@
     <div class="col-md-12">
       <div class="">
         <div class="x_content">
-          <div class="row">
-
-            <div class="animated flipInY col-lg-3 col-md-3 col-sm-6  ">
-              <div class="tile-stats">
-                <div class="icon"><i class="fa fa-caret-square-o-right"></i>
-                </div>
-                <div class="count">179</div>
-
-                <h3>Manual Payment</h3>
-                <p>Payment Not Yet Confirmed.</p>
-              </div>
-            </div>
-
-            <div class="animated flipInY col-lg-3 col-md-3 col-sm-6  ">
-              <div class="tile-stats">
-                <div class="icon"><i class="fa fa-caret-square-o-right"></i>
-                </div>
-                <div class="count">179</div>
-
-                <h3>Auto Payment</h3>
-                <p>Incoming Auto Payment.</p>
-              </div>
-            </div>
-
-            <div class="animated flipInY col-lg-3 col-md-3 col-sm-6  ">
-              <div class="tile-stats">
-                <div class="icon"><i class="fa fa-caret-square-o-right"></i>
-                </div>
-                <div class="count">179</div>
-
-                <h3>New Message</h3>
-                <p>New Message From User.</p>
-              </div>
-            </div>
-
-            <div class="animated flipInY col-lg-3 col-md-3 col-sm-6  ">
-              <div class="tile-stats">
-                <div class="icon"><i class="fa fa-caret-square-o-right"></i>
-                </div>
-                <div class="count">179</div>
-
-                <h3>New Transaction</h3>
-                <p>New Transaction Today.</p>
-              </div>
-            </div>
-
-            
-          </div>
         </div>
       </div>
     </div>
